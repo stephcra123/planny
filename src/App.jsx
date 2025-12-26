35e446ab10e4a83a528ae949ae6d58dc3800d0a1
stephcra123 <186585465+stephcra123@users.noreply.github.com>
2025-12-25 21:30:37 -0500

    Implement todo completion feature and improve form handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import { useState } from 'react'
 function App() {
   const [todoList, setTodoList] = useState([])
   function addTodo(title){
-        const newTodo = {id: Date.now(), title: title}
+        const newTodo = {id: Date.now(), title: title, isCompleted: false}
         setTodoList([...todoList, newTodo])
   }
+  function completeTodo(todoId) {
+    const updatedTodos = todoList.map((todo) => {
+        if (todo.id === todoId) {
+        return { ...todo, isCompleted: true };
+    }
+        return todo;
+    });
+  setTodoList(updatedTodos);
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList}/>
+      <TodoList 
+        todoList={todoList}
+        onCompleteTodo={completeTodo}/>
     </div>
   )
 }
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,27 @@
 import { useRef } from "react"
+import { useState } from 'react'
 
 function TodoForm({onAddTodo}) {
     const todoTitleInput = useRef("");
+    const [workingTodoTitle, setWorkingTodoTitle] = useState('');
     function handleAddTodo(event){
         event.preventDefault()
-        //console.dir(event.target.title)
-        const title = event.target.title.value
-        onAddTodo(title)
+        //const title = event.target.title.value
+        onAddTodo(workingTodoTitle)
+        setWorkingTodoTitle('');
         todoTitleInput.current.focus()
-        event.target.title.value = ""
+        //event.target.title.value = ""
         
     }
     return(
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
-            <input id="todoTitle" name= "title" ref = {todoTitleInput}/> 
-            <button>Add Todo</button>
+            <input id="todoTitle" name= "title" 
+                ref = {todoTitleInput} 
+                value = {workingTodoTitle}
+                onChange={(e) => setWorkingTodoTitle(e.target.value)}
+            /> 
+            <button disabled={workingTodoTitle === ""}>Add Todo</button>
 
         </form>
     )
