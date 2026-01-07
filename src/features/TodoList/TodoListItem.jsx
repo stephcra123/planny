0f57b91628a21cd7d5f9495bd1347d8efbc049f8
stephcra123 <186585465+stephcra123@users.noreply.github.com>
2026-01-06 20:08:48 -0500

    Made editable

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react"
+import TextInputWithLabel from "../../shared/TextInputWithLabel.jsx"
+
 function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [workingTitle, setWorkingTitle] = useState(todo.title)
+  
+  function handleCancel() {
+    setWorkingTitle(todo.title)
+    setIsEditing(false)
+  }
+
+  function handleEdit(event) {
+  setWorkingTitle(event.target.value)
+  }
+
   return (
     <li>
       <form>
-        <input
-          type = "checkbox"
-          checked ={todo.isCompleted}
-          onChange={()=>onCompleteTodo(todo.id)}
-        />
-        {todo.title}
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit}/>
+            <button
+              type="button"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+            </>
+        ):(
+          <>
+            <label>
+              <input
+              type = "checkbox"
+              checked ={todo.isCompleted}
+              onChange={()=>onCompleteTodo(todo.id)}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
       </form>
     </li>
   );
