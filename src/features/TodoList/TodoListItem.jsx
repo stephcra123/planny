41e6e2d54fefe25def250ff34cb86146047c2b37
stephcra123 <186585465+stephcra123@users.noreply.github.com>
2026-01-06 20:32:25 -0500

    added save functionality

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import TextInputWithLabel from "../../shared/TextInputWithLabel.jsx"
 
-function TodoListItem({ todo, onCompleteTodo }) {
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
   const [isEditing, setIsEditing] = useState(false)
   const [workingTitle, setWorkingTitle] = useState(todo.title)
   
@@ -14,6 +14,13 @@ function TodoListItem({ todo, onCompleteTodo }) {
   setWorkingTitle(event.target.value)
   }
 
+  function handleUpdate(event) {
+    if(!isEditing) return;
+    event.preventDefault();
+    onUpdateTodo({ ...todo, title: workingTitle });
+    setIsEditing(false);
+  }
+ 
   return (
     <li>
       <form>
@@ -25,6 +32,12 @@ function TodoListItem({ todo, onCompleteTodo }) {
               onClick={handleCancel}
             >
               Cancel
+            </button>
+            <button
+              type="button"
+              onClick={handleUpdate}
+            >
+              Update
             </button>
             </>
         ):(
